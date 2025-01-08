a7bc677e27bfef4e40f03a616d82a0d6e06ba259
H82M19\Student <Student>
2025-01-08 08:11:53 +0700

    Bai 3

---
+++ Bai3.js
@@ -0,0 +1,46 @@
+
+var dolphinsData1 = [96, 108, 89];
+var koalasData1 = [88, 91, 110];
+
+var dolphinsAvg1 = (dolphinsData1[0] + dolphinsData1[1] + dolphinsData1[2]) / 3;
+var koalasAvg1 = (koalasData1[0] + koalasData1[1] + koalasData1[2]) / 3;
+
+if (dolphinsAvg1 > koalasAvg1 && dolphinsAvg1 >= 100) {
+  console.log("Dolphins danh duoc cup!");
+} else if (koalasAvg1 > dolphinsAvg1 && koalasAvg1 >= 100) {
+  console.log("Koalas danh duoc cup!");
+} else if (dolphinsAvg1 === koalasAvg1 && dolphinsAvg1 >= 100 && koalasAvg1 >= 100) {
+  console.log("Hoa!");
+} else {
+  console.log("Khong doi nao thang.");
+}
+var dolphinsBonus1 = [97, 112, 101];
+var koalasBonus1 = [109, 95, 123];
+
+var dolphinsAvg2 = (dolphinsBonus1[0] + dolphinsBonus1[1] + dolphinsBonus1[2]) / 3;
+var koalasAvg2 = (koalasBonus1[0] + koalasBonus1[1] + koalasBonus1[2]) / 3;
+
+if (dolphinsAvg2 > koalasAvg2 && dolphinsAvg2 >= 100) {
+  console.log("Dolphins danh duoc cup!");
+} else if (koalasAvg2 > dolphinsAvg2 && koalasAvg2 >= 100) {
+    console.log("Koalas danh duoc cup!");
+} else if (dolphinsAvg2 === koalasAvg2 && dolphinsAvg2 >= 100 && koalasAvg2 >= 100) {
+  console.log("Hoa!");
+} else {
+  console.log("Khong doi nao thang.");
+}
+var dolphinsBonus2 = [97, 112, 101];
+var koalasBonus2 = [109, 95, 106];
+
+var dolphinsAvg3 = (dolphinsBonus2[0] + dolphinsBonus2[1] + dolphinsBonus2[2]) / 3;
+var koalasAvg3 = (koalasBonus2[0] + koalasBonus2[1] + koalasBonus2[2]) / 3;
+
+if (dolphinsAvg3 > koalasAvg3 && dolphinsAvg3 >= 100) {
+  console.log("Dolphins danh duoc cup!");
+} else if (koalasAvg3 > dolphinsAvg3 && koalasAvg3 >= 100) {
+    console.log("Koalas danh duoc cup!");
+} else if (dolphinsAvg3 === koalasAvg3 && dolphinsAvg3 >= 100 && koalasAvg3 >= 100) {
+  console.log("Hoa!");
+} else {
+  console.log("Khong doi nao thang.");
+}
