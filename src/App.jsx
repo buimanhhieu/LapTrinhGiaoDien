bdba9b15657f5a9f48092fe9230db6d8c412b840
H82M19\Student <Student>
2025-02-19 08:54:13 +0700

    Bai2_Tuan2

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,29 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [text, setText] = useState('')
-  const [name, setName] = useState('')
-  function handleAssign(e){
-    setText(e.target.value)
+  const [texta, seta] = useState(0)
+  const [textb, setb] = useState(0)
+  const [sum, setSum] = useState(0)
+  function handleA(e){
+    seta(e.target.value);
+  }
+  function handleB(e){
+    setb(e.target.value);
   }
   function handleClick(){
-    setName(text);
+    let c = parseInt(texta)+parseInt(textb)
+    setSum(c);
   }
 
   return (
     <>
-      <input onChange={handleAssign} type="text"  />
-      <button onClick={handleClick}>Click</button>
-      <span>Hello: {name}</span>
+      <input onChange={handleA} type="text"  />
+      <br />
+      <input onChange={handleB} type="text"  />
+      <br />
+      <button onClick={handleClick}>Sum</button>
+      <br />
+      <span>{sum}</span>
     </>
   )
 }
