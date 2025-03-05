32e89f7a0e42a1c5edd252503404b9d436005010
H82M19\Student <Student>
2025-03-05 08:47:06 +0700

    Tuan 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import React from "react"
+import Header from "./components/header"
+import Footer from "./components/footer"
+import Content from "./components/content"
 
 function App() {
-  const [texta, seta] = useState(0)
-  const [textb, setb] = useState(0)
-  const [sum, setSum] = useState(0)
-  function handleA(e){
-    seta(e.target.value);
-  }
-  function handleB(e){
-    setb(e.target.value);
-  }
-  function handleClick(){
-    let c = parseInt(texta)+parseInt(textb)
-    setSum(c);
-  }
 
   return (
     <>
-      <input onChange={handleA} type="text"  />
-      <br />
-      <input onChange={handleB} type="text"  />
-      <br />
-      <button onClick={handleClick}>Sum</button>
-      <br />
-      <span>{sum}</span>
+      <div>
+        <Header/>
+        <Content/>
+        <Footer/>
+      </div>
     </>
   )
 }
